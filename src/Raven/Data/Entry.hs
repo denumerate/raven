7961ddf30d9d1f3a7f470a0a76a870ff586b378c
Paul Ricks <paulricks@gmail.com>
2017-01-23 01:56:34 -0600

    docs

--- src/Raven/Data/Entry.hs
+++ src/Raven/Data/Entry.hs
@@ -5,11 +5,18 @@ import qualified Data.Text as Text
 import Data.Text (Text)
 import Data.Typeable
 
+-- |Creates the requirements for an Entry which are:
+-- the entry must be able to go from a typeable input and back through
+-- buildEntry, which checks the input against possible values and declares them,
+-- and dumpEntry, which dumps the values
+-- and there must be an NA value to indicate missing or corrupted data
+-- and can be checked for with the isNA predicate
 class Entry a where
   buildEntry :: (Typeable b) => b -> a
   dumpEntry :: (Typeable b) => a -> b
   isNA :: a -> Bool
 
+-- |BasicEntry creates a simple entry with bounded values
 data BasicEntry = BasicInt Int
                 | BasicDouble Double
                 | BasicRatio (Ratio Int)
@@ -21,6 +28,7 @@ instance Entry BasicEntry where
   buildEntry val
     |
 
+-- | BasicUnboundEntry creates a simple entry with unbounded values
 data BasicUnboundEntry = BasicUnboundInt Integer
                        | BasicUnboundDouble Double
                        | BasicUnboundRatio (Ratio Integer)
