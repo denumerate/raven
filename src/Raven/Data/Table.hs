6a9552c3cde4e1bf4c7664de1093c5b3d167d24d
Paul Ricks <paulricks@gmail.com>
2017-02-05 14:18:51 -0600

    error message clean up

--- src/Raven/Data/Table.hs
+++ src/Raven/Data/Table.hs
@@ -28,6 +28,22 @@ data Table a = Table Titles (Vector (Vector a))
 
 type Error = Text
 
+-- Error Messages:
+columnLengthError :: Text
+columnLengthError = "Not all columns the same length"
+
+titleNumberError :: Text
+titleNumberError = "Number of titles does not match number of columns"
+
+outOfBoundsError :: Text
+outOfBoundsError = "Index out of bounds"
+
+titleNotFoundError :: Text
+titleNotFoundError = "Column title not found"
+
+vectorLengthError :: Text
+vectorLengthError = "Vector wrong length"
+
 -- |Create an empty table
 empty :: Table a
 empty = Table V.empty V.empty
@@ -38,26 +54,26 @@ empty = Table V.empty V.empty
 buildTable :: Titles -> Vector (Vector a) -> Either (Table a) Error
 buildTable titles vectors
   |V.null titles && V.null vectors = Left empty
-  |V.null titles || V.null vectors = Right "Number of titles does not match number of columns"
+  |V.null titles || V.null vectors = Right titleNumberError
   |V.length titles == V.length vectors =
      if all (\vec -> V.length vec == (V.length . V.head) vectors) $ V.tail vectors
         then Left $ Table titles vectors
-        else Right "Not all columns the same length"
-  |otherwise = Right "Number of titles does not match number of columns"
+        else Right columnLengthError
+  |otherwise = Right titleNumberError
 
 -- |Pulls a column from the table by column index
 -- O(1)
 getColByIndex :: Table a -> Int -> Either (Vector a) Error
 getColByIndex (Table _ tab) ind = case tab V.!? ind of
   Just vec -> Left vec
-  _ -> Right "Index out of bounds"
+  _ -> Right outOfBoundsError
 
 -- |Pulls a column from the table by title
 -- O(n) where n is the number of columns
 getColByTitle :: Table a -> Text -> Either (Vector a) Error
 getColByTitle tab@(Table ttls _) ttl = case V.findIndex (\val -> val == ttl) ttls of
   Just ind -> getColByIndex tab ind
-  _ -> Right "Column title not found"
+  _ -> Right titleNotFoundError
 
 -- |Adds a column to the end
 -- O(n) where n is the number of columns
@@ -66,7 +82,7 @@ addColumn t@(Table ttls tab) ttl vec
   |Raven.Data.Table.null t = buildTable (V.fromList [ttl]) (V.fromList [vec])
   |(V.length . V.head) tab == V.length vec =
      Left $ Table (V.snoc ttls ttl) (V.snoc tab vec)
-  |otherwise = Right "Vector wrong length"
+  |otherwise = Right vectorLengthError
 
 -- |Test if a table is empty O(1)
 null :: Table a -> Bool
@@ -82,7 +98,7 @@ dropColByIndex :: Table a -> Int -> Either (Table a) Error
 dropColByIndex t@(Table ttls cols) ind
   |ind < 0 ||
    ind >= nCols t ||
-   Raven.Data.Table.null t = Right "Index out of bounds"
+   Raven.Data.Table.null t = Right outOfBoundsError
   |otherwise = Left $ Table (V.ifilter ipred ttls) (V.ifilter ipred cols)
   where
     ipred i _ = i /= ind
@@ -94,4 +110,4 @@ dropColByTitle :: Table a -> Text -> Either (Table a) Error
 dropColByTitle t@(Table ttls _) ttl =
   case V.findIndex (\val -> val == ttl) ttls of
     Just ind -> dropColByIndex t ind
-    _ -> Right "Column title not found"
+    _ -> Right titleNotFoundError
