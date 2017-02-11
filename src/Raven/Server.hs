8e58581ed52fbf34ea3435796b38ac0c5f873dfa
Paul Ricks <paulricks@gmail.com>
2017-02-11 01:32:57 -0600

    echo working with nodes

--- src/Raven/Server.hs
+++ src/Raven/Server.hs
@@ -8,7 +8,6 @@ import Network.Transport
 import Network.Transport.TCP
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
-import Control.Distributed.Process.Serializable
 import Control.Concurrent
 import Control.Monad (forever,unless)
 
@@ -16,11 +15,13 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
+import Data.Binary
+import Data.Typeable
 
 import Raven.REPL
 
 newtype TestMsg = TestMsg [ByteString]
-  deriving (Serializable)
+  deriving (Binary,Typeable)
 
 -- |Start the server and listen for connections at the supplied ip:port number.
 -- Returns the address of the server endpoint (if successful)
@@ -37,7 +38,7 @@ initServer ip portNum = withSocketsDo $
                 (\pid ->
                     runProcess node
                     (spawnLocal (liftIO (listenAtEnd trans' pid end' Map.empty)) >>
-                     spawnLocal (forever (receiveWait [])) >>=
+                     spawnLocal (forever (receiveWait [match handleTest])) >>=
                      liftIO . putMVar pid>>
                      unless True (return ()))))
             _ -> putStrLn "Endpoint not initialized, Server Failed" >> --move to log
@@ -76,5 +77,15 @@ handleData :: Transport -> MVar ProcessId -> [ByteString] -> Connection -> IO ()
 handleData trans pid sentData conn = newLocalNode trans initRemoteTable >>=
   (\handleNode -> readMVar pid >>=
     (\pid' -> runProcess handleNode
-      (Control.Distributed.Process.send pid' (TestMsg sentData) >>
-      return ())))
+      (getSelfPid >>=
+      (\self -> Control.Distributed.Process.send pid' (self,TestMsg sentData)) >>
+      receiveWait [match (sendResult conn)])))
+
+-- |Takes the result of the servers work and sends it back to the client
+sendResult :: Connection -> TestMsg -> Process ()
+sendResult conn (TestMsg msg) =
+  liftIO $ Network.Transport.send conn msg >>
+  return ()
+
+handleTest :: (ProcessId,TestMsg) -> Process ()
+handleTest (pid,msg) = Control.Distributed.Process.send pid msg
