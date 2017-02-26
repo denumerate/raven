10ae1aa1752a379d43c7dc05d6d65c84aa551fbd
Paul Ricks <paulricks@gmail.com>
2017-02-26 09:58:26 -0600

    switching problems

--- src/Raven/Data/Prob/DistributionFuncs.Test.hs
+++ src/Raven/Data/Prob/DistributionFuncs.Test.hs
@@ -16,7 +16,8 @@ main = do
     else exitSuccess
 
 allTests :: Test
-allTests = TestList $ []
+allTests = TestList $ runtests (uniform M.empty) uniformDataEmpty
+           ++ runtests (uniform set1) uniformDataSet1
 
 --run tests with 1 input
 runtests :: (Eq b,Show b) => (a -> b) -> [(String,a,b)] -> [Test]
@@ -25,3 +26,20 @@ runtests f ls = map (uncurry (~:)) (createtest f ls)
 --create tests with 1 input
 createtest :: (Eq b,Show b) => (a -> b) -> [(String,a,b)] -> [(String,Test)]
 createtest f = map (\(s,x,y) -> (s,TestCase $ y @=? f x))
+
+--Test Values:
+set1 = M.fromList $ zip [0..4] $ take 5 $ repeat $ 1%5
+
+uniformDataEmpty :: [(String,Rational,Rational)]
+uniformDataEmpty =
+  [ ("uniform: empty",8,0)
+  , ("uniform: empty 1",-4,0)
+  ]
+
+uniformDataSet1 :: [(String,Rational,Rational)]
+uniformDataSet1 =
+  [ ("uniform: zero",-4,0)
+  , ("uniform: zero 1",5%4,0)
+  , ("uniform: norm",2%3,1%5)
+  , ("uniform: norm",4%5,1%5)
+  ]
