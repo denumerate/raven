2713aee1f5197e2b570a249b7d8497de402a0289
Paul Ricks <paulricks@gmail.com>
2017-03-14 17:26:57 -0500

    setup

--- src/Raven/Client/Connection.hs
+++ src/Raven/Client/Connection.hs
@@ -1,3 +1,22 @@
 module Raven.Client.Connection
-  (
+  ( listenAtEnd
   )where
+
+import Network.Transport
+import Graphics.UI.Gtk hiding (Action, backspace)
+
+listenAtEnd :: EndPoint -> TextBuffer -> IO ()
+listenAtEnd end buf =
+  receive end >>=
+  (\event -> case event of
+      ConnectionClosed _ ->
+        textBufferSetText buf "Connection Closed" >>
+        listenAtEnd end buf
+      EndPointClosed ->
+        textBufferSetText buf "Error: Endpoint Closed" >>
+        listenAtEnd end buf
+      ErrorEvent (TransportError _ err) ->
+        textBufferSetText buf
+             ("Connection Error: " ++ err) >>
+        listenAtEnd end buf
+      _ -> listenAtEnd end buf)
--- src/Raven/Client/GUI.hs
+++ src/Raven/Client/GUI.hs
@@ -3,35 +3,48 @@ module Raven.Client.GUI
   )where
 
 import Network.Transport
+import Control.Concurrent
 
 import Control.Monad.IO.Class
 import Control.Monad
 import Graphics.UI.Gtk hiding (Action, backspace)
+
+import Raven.Client.Connection
 
 -- |Builds and runs ui
 guiMain :: Connection -> EndPoint -> EndPointAddress -> IO ()
 guiMain conn end server = void initGUI >>
   windowNew >>=
   (\w ->
-     windowSettings w >>
-     buildConnInfo server >>=
-     containerAdd w >>
+     windowSettings conn w >>
+     buildConnInfo end server >>=
+     (\connL ->
+        vBoxNew False 2 >>=
+        (\vbox ->
+           boxPackStart vbox connL PackNatural 2 >>
+           buildWork >>=
+           (\ws -> boxPackEnd vbox ws PackGrow 2) >>
+           containerAdd w vbox)) >>
      widgetShowAll w) >>
   mainGUI
 
 -- |window settings
-windowSettings :: Window -> IO ()
-windowSettings w =
+windowSettings :: Connection -> Window -> IO ()
+windowSettings conn w =
   set w [ windowTitle := "raven"
         ]>>
   windowFullscreen w >>
-  on w deleteEvent (liftIO mainQuit >> return False) >>
+  on w deleteEvent (liftIO (close conn) >>
+                    liftIO mainQuit >>
+                    return False) >>
   return ()
 
-buildConnInfo :: EndPointAddress -> IO TextView
-buildConnInfo server =
+-- |Creates label that displays connection info and starts up the listening process
+buildConnInfo :: EndPoint -> EndPointAddress -> IO TextView
+buildConnInfo end server =
   textBufferNew Nothing >>=
   (\tbuf ->
+     forkIO (listenAtEnd end tbuf) >>
      textBufferSetText tbuf ("Connection Established: " ++ (show server)) >>
      textViewNewWithBuffer tbuf >>=
      (\tview ->
@@ -39,3 +52,29 @@ buildConnInfo server =
                   , textViewEditable := False
                   ] >>
         return tview))
+
+-- |Creates the widget that allows for work
+buildWork :: IO HBox
+buildWork =
+  hBoxNew False 2 >>=
+  (\vbox ->
+     vScrollbarNewDefaults >>=
+     (\s -> boxPackEnd vbox s PackNatural 2 >>
+            inOuts >>=
+            (\ios -> boxPackStart vbox ios PackGrow 2)) >>
+     return vbox)
+
+-- |creates input output areas
+inOuts :: IO HBox
+inOuts =
+  hBoxNew False 2 >>=
+  (\hbox -> inOut >>=
+    (\io -> boxPackStart hbox io PackGrow 2 >>
+            return hbox))
+  where
+    inOut = hBoxNew False 2 >>=
+      (\hbox -> textViewNew >>=
+        (\i -> boxPackStart hbox i PackGrow 2 >>
+               textViewSetCursorVisible i True >>
+               textViewSetWrapMode i WrapWordChar >>
+               return hbox))
