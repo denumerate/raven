d565a82d9a5e5885153b0d777a527cc24feb66fc
Paul Ricks <paulricks@gmail.com>
2017-05-04 22:31:53 -0500

    placing returns in correct widget

--- src/Raven/Client/Connection.hs
+++ src/Raven/Client/Connection.hs
@@ -4,35 +4,39 @@ module Raven.Client.Connection
   )where
 
 import Network.Transport
-import Graphics.UI.Gtk hiding (Action, backspace)
+import Control.Concurrent
 
+import Graphics.UI.Gtk hiding (Action, backspace)
 import Codec.Picture
 
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Text.Read
 
 import Control.Monad (void)
 
 -- |Listens at endpoint and handles events
-listenAtEnd :: EndPoint -> TextBuffer -> TextBuffer -> IO ()
-listenAtEnd end connbuf workbuf =
+listenAtEnd :: EndPoint -> TextBuffer -> MVar (Vector TextBuffer) -> IO ()
+listenAtEnd end connbuf workbufs =
   receive end >>=
   (\event -> case event of
       ConnectionClosed _ ->
         textBufferSetText connbuf "Connection Closed" >>
-        listenAtEnd end connbuf workbuf
+        listenAtEnd end connbuf workbufs
       EndPointClosed ->
         textBufferSetText connbuf "Error: Endpoint Closed" >>
-        listenAtEnd end connbuf workbuf
+        listenAtEnd end connbuf workbufs
       ErrorEvent (TransportError _ err) ->
         textBufferSetText connbuf
              ("Connection Error: " ++ err) >>
-        listenAtEnd end connbuf workbuf
-      Received _ [val] -> modBuf workbuf val >>
-        listenAtEnd end connbuf workbuf
+        listenAtEnd end connbuf workbufs
+      Received _ [val] -> modBuf workbufs val >>
+        listenAtEnd end connbuf workbufs
       Received _ vals -> buildImg vals >>
-        listenAtEnd end connbuf workbuf
-      _ -> listenAtEnd end connbuf workbuf)
+        listenAtEnd end connbuf workbufs
+      _ -> listenAtEnd end connbuf workbufs)
 
 -- |Sends a request to the server.
 -- Needs the connection, the line number, and the message
@@ -40,10 +44,15 @@ sendReq :: Connection -> Int -> ByteString -> IO ()
 sendReq conn n val = void $ send conn [B.pack $ show n,B.pack " ",val] --error possible
 
 -- |updates the work buffer
-modBuf :: TextBuffer -> ByteString -> IO ()
-modBuf buf str =
-  textBufferInsertByteStringAtCursor buf $
-  B.concat [str,B.pack "\n"]
+modBuf :: MVar (Vector TextBuffer) -> ByteString -> IO ()
+modBuf bufs str = let wds = B.words str in
+  case (readMaybe (B.unpack (head wds)) :: Maybe Int) of
+    Just n -> readMVar bufs >>=
+      (\bufs' -> let buf = bufs' V.! n in
+          textBufferGetEndIter buf >>=
+          (\iter ->
+             textBufferInsertByteString buf iter (B.unwords (tail wds))))
+    _ -> return ()
 
 buildImg :: [ByteString] -> IO ()
 buildImg vals = let vals' = B.concat vals
